4fa1125208b960e6a8948260084088a0b1c2dac1
EasonYao12 <yia7617@gmail.com>
2019-06-14 13:50:31 +0800

    Update fss.sh

--- fss.sh
+++ fss.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=12
-#export MP_STACKSIZE=8G
+export OMP_NUM_THREADS=12 #the core you want to use ; max = 12
 
 pgf90 -mp -O3 -o FSS_omp.exe fss.f90
 ./FSS_omp.exe
